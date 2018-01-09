577272579a6c37218498e4451eb1211d1e6fa8bc
Leif Gensert <leif@leif.io>
2018-01-09 14:55:26 +0700

    name variable correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ do
   conf=${base%.*}
 
   echo "Writing conf: ${conf}"
-  envsubst '${RELEASE_PING_APP_ENV_DOCKERCLOUD_SERVICE_HOSTNAMER1},${RELEASE_PING_APP_ENV_PORT}' < ${f} > /etc/nginx/sites-enabled/${conf}
+  envsubst '${RELEASE_PING_APP_ENV_DOCKERCLOUD_SERVICE_HOSTNAME},${RELEASE_PING_APP_ENV_PORT}' < ${f} > /etc/nginx/sites-enabled/${conf}
   cat /etc/nginx/sites-enabled/${conf}
 done
 
